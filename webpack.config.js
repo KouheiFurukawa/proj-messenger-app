be6873f6ce1154358803a88b8532ee318ee23f17
Kouhei <furukawabashi42@gmail.com>
2020-03-28 13:22:41 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.tsx',
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                test: /\.tsx?$/,
+                exclude: [
+                    /node_modules/
+                ],
+                options: {
+                    emitErrors: true
+                }
+            },
+            {
+                loader: 'ts-loader',
+                test: /\.tsx?$/,
+                exclude: [
+                    /node_modules/
+                ],
+                options: {
+                    configFile: 'tsconfig.json'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'static/js/[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            chunks: ['main'],
+            filename: "index.html",
+            template: "index.html"
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        inline: true,
+        host: 'localhost',
+        port: 8080,
+    }
+};
