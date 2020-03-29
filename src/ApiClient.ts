1ca09bd6e686fc75d50ca00e1adf07aba89c2fd5
Kouhei <furukawabashi42@gmail.com>
2020-03-29 12:43:49 +0900

    feat: implement apiclient

---
+++ src/ApiClient.ts
@@ -0,0 +1,40 @@
+export default class ApiClient {
+    static getUser() {
+        return fetch('/get_user/')
+            .then((response) => response.json())
+            .then((data) => {
+                return { result: data };
+            })
+            .catch((error) => {
+                return { error };
+            });
+    }
+
+    static getMessage(params: { user1: string; user2: string }) {
+        return fetch(`/get_message?user1=${params.user1}&user2=${params.user2}`)
+            .then((response) => response.json())
+            .then((data) => {
+                return { result: data };
+            })
+            .catch((error) => {
+                return { error };
+            });
+    }
+
+    static sendMessage(params: { user_from: string; user_to: string; text: string; send_date: string }) {
+        return fetch('/send_message/', {
+            method: 'POST',
+            body: JSON.stringify({ ...params }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return { result: data };
+            })
+            .catch((error) => {
+                return { error };
+            });
+    }
+}
