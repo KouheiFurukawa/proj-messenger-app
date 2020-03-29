2b2727183eaea3856f50189e18fe7b6cf39313c0
Kouhei <furukawabashi42@gmail.com>
2020-03-29 12:30:35 +0900

    feat: implement server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'MessengerApp',
+});
+
+app.get('/get_user/', (req, res) => {
+    connection.query('select * from user', (error, results, fields) => {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+app.get('/get_message/', (req, res) => {
+    connection.query('select * from message where (user_from = \'test1\' and user_to = \'test2\') or (user_from = \'test2\' and user_to = \'test1\')', (error, results, fields) => {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+app.post('/send_message/', (req, res) => {
+    const {user_from, user_to, text, send_date} = req.body;
+    connection.query(`insert into message(user_from,user_to,text,send_date) values ('${user_from}','${user_to}','${text}',${send_date})`, (error, results, fields) => {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
+
+module.exports = {
+    path: '/server',
+    handler: app
+};
